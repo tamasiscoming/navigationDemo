59dd93aa426ee9d910e3fcf1f8931b315a93a0c2
Nguyen Tamás <nvntamas@gmail.com>
2026-03-21 01:44:24 +0100

    Initial Commit

---
+++ NavigationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationDemo
+//
+//  Created by Nguyen Van Nam Tamás on 2026. 03. 21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by Nguyen Van Nam Tamás on 2026. 03. 21..
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
